08cf3f8a647e0ae18322b13bd28281ea215dc9a7
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-11 13:55:45 +0530

    UI of login page

---
+++ src/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import AmazonLogo from "./images/amazon-logo.png";
+
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <Link to="/">
+        <img className="login__logo" src={AmazonLogo} />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By signing-in you agree to AMAZON FAKE CLONE'S Conditions of Use &
+          Sale, Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
